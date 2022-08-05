0489a5479c240106005aa3981f08f8fc36ff83ce
LeoZion <leozionsuporte@gmail.com>
2022-08-05 02:16:37 -0300

    binding

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ const app = {
     return {
       name: 'Jhon',
       lastname: 'Doe',
+      product: {
+        title: 'Pulp Fiction',
+        description: 'Lorem ipsum Pulp Fictions sit dollores',
+        image: './assets/images/pulp-fiction.jpg',
+
+      },
+      product2: {
+        title: 'Pulp Fiction',
+        description: 'Lorem ipsum Pulp Fictions sit dollores',
+        image: './assets/images/quanto-mais-idiota-melhor.jpg',
+
+      }
     }
   }
 }
