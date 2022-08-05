2d752e58e0f9299e897cbeb1fe08e8a6f7e33e26
LeoZion <leozionsuporte@gmail.com>
2022-08-05 02:32:01 -0300

    loop v-for

--- app.js
+++ app.js
@@ -3,19 +3,22 @@ const app = {
     return {
       name: 'Jhon',
       lastname: 'Doe',
-      product: {
-        title: 'Pulp Fiction',
-        description: 'Lorem ipsum Pulp Fictions sit dollores',
-        image: './assets/images/pulp-fiction.jpg',
-        stars: 3
-
-      },
-      product2: {
-        title: 'Pulp Fiction',
-        description: 'Lorem ipsum Pulp Fictions sit dollores',
-        image: './assets/images/quanto-mais-idiota-melhor.jpg',
-        stars: 0
-      }
+      products: [
+        {
+          id: 1,
+          title: 'Pulp Fiction',
+          description: 'Lorem ipsum Pulp Fictions sit dollores',
+          image: './assets/images/pulp-fiction.jpg',
+          stars: 3  
+        },
+        {
+          id: 2,
+          title: 'Quanto mais idiota melhor',
+          description: 'Lorem ipsum Pulp Fictions sit dollores',
+          image: './assets/images/quanto-mais-idiota-melhor.jpg',
+          stars: 0
+        },
+      ]
     }
   }
 }
