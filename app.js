802097873d1e58369ec552566c1b9a1ca3d048cc
LeoZion <leozionsuporte@gmail.com>
2022-08-05 02:24:20 -0300

    conditional rendering

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const app = {
         title: 'Pulp Fiction',
         description: 'Lorem ipsum Pulp Fictions sit dollores',
         image: './assets/images/pulp-fiction.jpg',
+        stars: 3
 
       },
       product2: {
         title: 'Pulp Fiction',
         description: 'Lorem ipsum Pulp Fictions sit dollores',
         image: './assets/images/quanto-mais-idiota-melhor.jpg',
-
+        stars: 0
       }
     }
   }
